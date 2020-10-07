cec26b428bcd186c1455be3f14ac9ccb48adda4c
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-10-07 09:16:52 +0530

    [Shreya]Added:Uc1

--- Tictactoe.java
+++ Tictactoe.java
@@ -3,6 +3,19 @@ package com.example;
 public class Tictactoe {
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe");
+		initializeBoard();
+	}
+	
+	private static String[] board = new String[10];
+	
+	public static void initializeBoard() { 
+	    for (int i = 0; i < board.length; i++) {
+	       
+	            board[i] = "-";
+	            System.out.println(board[i]);
+	        
+	    }
+	    System.out.println("Board is empty ");
 	}
 
 }
